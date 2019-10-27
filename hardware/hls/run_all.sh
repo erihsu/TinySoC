a7382101424a1730da982f48b3c9a3ca7f4008fd
eirc <zhenyuxu163@163.com>
2019-10-27 15:12:45 +0800

    update hardware

--- hardware/hls/run_all.sh
+++ hardware/hls/run_all.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 echo "start generating Binary circuts with vivado_hls"
-bash ./Binary/make_IP.sh
+# change directory to Binary
+cd ./Binary 
+bash ./make_IP.sh
+# go back to hls
+cd ..
 echo "exit from generating"
 
 echo "-------------------------------------------"
 
 echo "start generating CNN circuits with vivado_hls"
-bash ./HLS_ACC/make_IP.sh
+# change directory to HLS_ACC
+cd ./HLS_ACC 
+bash ./make_IP.sh
+# go back to hls
+cd ..
 echo "exit from generating"
